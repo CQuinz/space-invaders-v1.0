b51a58afada76d1500a68ce9b49a8bc130469ead
Christian <christian.quinlan36@gmail.com>
2021-05-10 12:53:06 +0100

    Adding inital js script to move player img left or right

---
+++ script.js
@@ -0,0 +1,48 @@
+const keyCodeLeft = 37;
+const keyCodeRight = 39;
+const keyCodeSpace = 32;
+
+const gameWidth = 800;
+const gameHeight = 600;
+
+const gameState = {
+  playerX: 0,
+  playerY: 0
+};
+
+const setPosition = (element, xPos, yPos)=>{
+  element.style.transform = `translate(${xPos}px, ${yPos}px)`;
+}
+
+const createPlayer = (container)=>{
+  gameState.playerX = gameWidth/2;
+  gameState.playerY = gameHeight - 50;
+
+  const player = document.createElement('img');
+  player.src= 'images/playerShip1_orange.png';
+  player.className = 'player';
+  container.appendChild(player);
+
+  setPosition(player, gameState.playerX, gameState.playerY);
+}
+
+const init = ()=>{
+  const container = document.querySelector('.game');
+  createPlayer(container);
+}
+
+
+const onKeyDown = (e)=> {
+  if(e.keyCode === keyCodeLeft){
+    gameState.playerX -=5;
+    const player = document.querySelector('.player');
+    setPosition(player, gameState.playerX, gameState.playerY)
+  }else if(e.keyCode === keyCodeRight){
+    gameState.playerX += 5;
+    const player = document.querySelector('.player');
+    setPosition(player, gameState.playerX, gameState.playerY)
+  }
+}
+
+init();
+window.addEventListener('keydown', onKeyDown);
